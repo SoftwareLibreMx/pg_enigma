b0882c283189fc696625462c057fb36a0693174a
Iván Chavero <ichavero@chavero.com.mx>
2024-05-24 22:50:40 -0600

    Add functions for extracting and inserting to postgres

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,70 @@
 use pgrx::prelude::*;
+use serde::{Serialize, Deserialize};
+use std::fmt::{Display, Formatter};
+use pgrx::{StringInfo};
+use core::ffi::CStr;
 
 ::pgrx::pg_module_magic!();
 
-#[pg_extern]
-fn hello_pg_enigma() -> &'static str {
-    "Hello, pg_enigma"
+//TODO: temporal key for testing
+
+static KEY: Option<&str> = Some("SUPER SECRET KEY");
+
+/// Value stores entcrypted information
+#[derive(Serialize, Deserialize, PostgresType)]
+#[inoutfuncs]
+struct Enigma {
+    value: String,
 }
+
+/// Functions for extracting and inserting data
+impl InOutFuncs for Enigma {
+    // Get from postgres
+    fn input(input: &CStr) -> Self {
+        let mut value: String = input
+                .to_str()
+                .expect("Enigma::input can't convert to str")
+                .to_string();
+
+        // TODO: better error handling
+        if KEY != None {
+            value = encrypt(value);
+        } else {
+            value = format!("NO KEY DEFINED");
+        }
+
+        Enigma {
+            value: value.clone(),
+        }
+    }
+
+    // Send to postgres
+    fn output(&self, buffer: &mut StringInfo) {
+
+        let mut value: String = self.value.clone();
+
+        // TODO: better error handling
+        if KEY != None {
+            value = decrypt(value);
+        } else {
+            value = format!("NO KEY DEFINED THIS VALUE IS ENCRYPTED: {}", value);
+        }
+
+        buffer.push_str(&format!("{}", value));
+    }
+}
+
+/// Encrypts the value
+pub fn decrypt(value: String) -> String {
+    format!("DECRYPTED {}", value)
+}
+
+/// Decrypts the value
+pub fn encrypt(value: String) -> String {
+    format!("ENCRYPTED {}", value)
+}
+
+
 
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
