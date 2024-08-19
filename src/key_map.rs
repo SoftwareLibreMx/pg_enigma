7f697ce7b00c7e27f3bc8ae546a57dd0791f769b
Sandino Araico Sanchez <sandino@sandino.net>
2024-08-19 00:26:34 -0600

    pub_key module based on priv_key

--- src/key_map.rs
+++ src/key_map.rs
@@ -1,7 +1,11 @@
 use std::collections::BTreeMap;
 use crate::priv_key::PrivKey;
+use crate::pub_key::PubKey;
 use std::sync::RwLock;
 
+/********************
+ * Private keys map *
+ * ******************/
 pub struct PrivKeysMap {
     /// each `BTreeMap` entry is a reference to a `PrivKey` structure
     keys: RwLock<BTreeMap<i32,&'static PrivKey>>,
@@ -92,6 +96,105 @@ impl PrivKeysMap {
             Some(sec_key) => {
                 let decrypted = sec_key.decrypt(value)?;
                 Ok(Some(decrypted))
+            },
+            None => Ok(None)
+        }
+    }
+}
+
+/*******************
+ * Public keys map *
+ * *****************/
+pub struct PubKeysMap {
+    /// each `BTreeMap` entry is a reference to a `PubKey` structure
+    keys: RwLock<BTreeMap<i32,&'static PubKey>>,
+}
+
+/// Functions for private keys map
+/// Lifetimes are handled here, so these functions can be called safely
+/// from elsewhere.
+impl PubKeysMap {
+    /// Creates new (empty) PubKeys struct
+    pub fn new() -> Self {
+        let keys = RwLock::new(BTreeMap::new());
+        PubKeysMap {
+            keys: keys // new empty BTreeMap
+        }
+    }
+
+    /// Sets the `PubKeysMap` `id` to the `PubKey` obtained from the
+    /// provides armored key and plain text password
+    pub fn set(&self, id: i32, armored_key: &str, pw: &str)
+    -> Result<String, Box<(dyn std::error::Error + 'static)>> {
+        let key = PubKey::new(armored_key)?; // key with '1 lifetime
+        let key_id = key.key_id();
+        // put the key into the box to allow change it's lifetime
+        let boxed_key = Box::new(key);
+        // leaked key is the same address, but now with 'static lifetime
+        let static_key: &'static PubKey = Box::leak(boxed_key);
+        // need write lock to insert the key on the BTreeMap
+        let old = match self.keys.write() {
+            // RwLock::insert() returns Some(old_value) if replaced
+            Ok(mut m) => m.insert(id, &static_key),
+            Err(e) => return Err(
+                format!("PubKeysMap: set: could not get write lock: {}", e)
+                .into()),
+        };
+        
+        let msg = match old {
+            Some(o) => { // the old key was replaced
+                let old_id = o.key_id(); 
+                // TODO: drop(o); // free old key (explicitly)
+                format!("key {}: public key {} replaced with {}", 
+                    id, old_id, key_id)
+            },
+            None => { // No previous key was replaced
+                format!("key {}: public key {} imported", id, key_id)
+            }
+        };
+        Ok(msg)
+    }
+
+    pub fn del(&'static self, id: i32) 
+    -> Result<String, Box<(dyn std::error::Error + 'static)>> {
+        let old = match self.keys.write() {
+            Ok(mut m) => {
+                m.remove(&id)
+            },
+            Err(e) => return Err(
+                format!("PubKeysMap: del: could not get write lock: {}", e)
+                .into()),
+        };
+
+        let msg = match old {
+            Some(o) => {
+                let key_id = o.key_id();
+                // TODO: drop(o); // free old key (explicitly)
+                format!("key {}: public key {} forgotten", id, key_id)
+            },
+            None => format!("key {}: not set", id)
+        };
+        Ok(msg)
+    }
+
+    /// Gets reference to `PubKey` from `PubKeysMap` entry with `id` 
+    pub fn get(self: &'static PubKeysMap, id: i32) 
+    -> Result<Option<&'static PubKey>, 
+    Box<(dyn std::error::Error + 'static)>> {
+        let binding = self.keys.read()?;
+        let key = match binding.get(&id) {
+            Some(k) => k,
+            None => return Ok(None)
+        };
+        Ok(Some(key))
+    }
+
+    pub fn encrypt(self: &'static PubKeysMap, id: i32, value: &String)
+    -> Result<Option<String>, Box<(dyn std::error::Error + 'static)>> {
+        match self.get(id)? {
+            Some(key) => {
+                let encrypted = key.encrypt(value)?;
+                Ok(Some(encrypted))
             },
             None => Ok(None)
         }
