2607c5f1ca2850574246416bc65270c8bf3ef3c6
Sandino Araico Sanchez <sandino@sandino.net>
2025-06-24 21:01:51 -0600

    table _enigma_public_keys created on bootstrap

--- src/functions.rs
+++ src/functions.rs
@@ -7,8 +7,8 @@ use pgrx::datum::DatumWithOid;
 // TODO: move to key_map module
 /// Get the public key from the keys table
 pub fn get_public_key(id: i32) -> Result<Option<String>, pgrx::spi::Error> {
-    if ! exists_key_table()? { return Ok(None); }
-    let query = "SELECT public_key FROM enigma_public_keys WHERE id = $1";
+    // if ! exists_key_table()? { return Ok(None); }
+    let query = "SELECT public_key FROM _enigma_public_keys WHERE id = $1";
     let args = unsafe { 
         [ DatumWithOid::new(id, PgBuiltInOids::INT4OID.value()) ]
     };
@@ -23,7 +23,7 @@ pub fn get_public_key(id: i32) -> Result<Option<String>, pgrx::spi::Error> {
 
 }
 
-// TODO: this function might be no longer needed
+/* // TODO: this function might be no longer needed
 pub fn exists_key_table() -> Result<bool, pgrx::spi::Error> {
     if let Some(e) = Spi::get_one("SELECT EXISTS (
         SELECT tablename
@@ -36,7 +36,7 @@ pub fn exists_key_table() -> Result<bool, pgrx::spi::Error> {
     Ok(false)
 }
 
-// TODO: extension_sql! before CREATE TYPE
+TODO: extension_sql! before CREATE TYPE
 // TODO: rename table to _enigma_public_keys
 pub fn create_key_table() -> Result<(), pgrx::spi::Error> {
     Spi::run(
@@ -45,12 +45,12 @@ pub fn create_key_table() -> Result<(), pgrx::spi::Error> {
             public_key TEXT
          )"
     )
-}
+} */
 
 // TODO: move to key_map module
 pub fn insert_public_key(id: i32, key: &str)
 -> Result<Option<String>, pgrx::spi::Error> {
-    create_key_table()?;
+    // create_key_table()?;
     let args = unsafe {
         [
             DatumWithOid::new(id,  PgBuiltInOids::INT4OID.value()),
@@ -58,7 +58,7 @@ pub fn insert_public_key(id: i32, key: &str)
         ]
     };
     Spi::get_one_with_args(
-        r#"INSERT INTO enigma_public_keys(id, public_key)
+        r#"INSERT INTO _enigma_public_keys(id, public_key)
            VALUES ($1, $2)
            ON CONFLICT(id)
            DO UPDATE SET public_key=$2
--- src/lib.rs
+++ src/lib.rs
@@ -195,6 +195,10 @@ fn set_public_key_from_file(id: i32, file_path: &str)
 // Create the type manually
 extension_sql!(
     r#"
+        CREATE TABLE IF NOT EXISTS _enigma_public_keys (
+            id INT PRIMARY KEY,
+            public_key TEXT 
+        );
         CREATE TYPE enigma;
     "#,
     name = "shell_type",
@@ -308,7 +312,7 @@ SELECT set_private_key(2, '--- INVALID KEY ---', 'bad pass');
 SELECT set_public_key_from_file(2, '../../../test/public-key.asc'); 
         ")?;
         if let Some(res) = Spi::get_one::<String>("
-SELECT public_key FROM enigma_public_keys WHERE id = 2;
+SELECT public_key FROM _enigma_public_keys WHERE id = 2;
         ")? {
             if res.contains("BEGIN PGP PUBLIC KEY") { return Ok(()); }
         } 
